587c03f7abde2b4fb85ff305bee9e9b9ccbd19a3
Lucas Oliveira <lucasio2008@gmail.com>
2025-07-15 15:53:21 -0300

    feat: Adiciona o arquivo principal da aplicação.

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from api.endpoints.auth.router import router as auth_router
+from api.endpoints.orders.router import router as orders_router
+
+app = FastAPI(
+    title='FastAPI',
+    description='API para gerenciamento de pedidos',
+    version='0.1.0',
+)
+
+app.include_router(auth_router)
+app.include_router(orders_router)
