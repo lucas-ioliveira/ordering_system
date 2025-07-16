c0435a7eaae85e5cdf4ad3e973b17d99ae76186e
Lucas Oliveira <lucasio2008@gmail.com>
2025-07-16 15:18:54 -0300

    feat: Adiciona rota itens do pedido.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from api.endpoints.auth.router import router as auth_router
 from api.endpoints.orders.router import router as orders_router
+from api.endpoints.order_items.router import router as order_items_router
 
 app = FastAPI(
     title='FastAPI',
@@ -11,3 +12,4 @@ app = FastAPI(
 
 app.include_router(auth_router)
 app.include_router(orders_router)
+app.include_router(order_items_router)
